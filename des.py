3c221cbcc43e465271629fc5df5b57fb28a32fcf
includeavaneesh <avaneeshkanshi5@gmail.com>
2022-03-07 16:18:23 +0530

    aes des

---
+++ des.py
@@ -0,0 +1,49 @@
+import hashlib
+from Crypto import Random
+from Crypto.Cipher import DES
+from Crypto.Random import get_random_bytes
+from base64 import b64encode, b64decode
+
+class AESCipher(object):
+    def __init__(self, key):
+        self.block_size = DES.block_size
+        # print(self.block_size)
+        # self.key = hashlib.sha256(key.encode()).digest()
+        self.key = key
+
+    def encrypt(self, plain_text):
+        plain_text = self.__pad(plain_text)
+        iv = Random.new().read(self.block_size)
+        print(str(iv))
+        cipher = DES.new(self.key, DES.MODE_CBC, iv)
+        encrypted_text = cipher.encrypt(plain_text.encode())
+        return b64encode(iv + encrypted_text).decode("utf-8")
+
+    def decrypt(self, encrypted_text):
+        encrypted_text = b64decode(encrypted_text)
+        iv = encrypted_text[:self.block_size]
+        cipher = DES.new(self.key, DES.MODE_CBC, iv)
+        plain_text = cipher.decrypt(encrypted_text[self.block_size:]).decode("utf-8")
+        return self.__unpad(plain_text)
+
+    def __pad(self, plain_text):
+        number_of_bytes_to_pad = self.block_size - len(plain_text) % self.block_size
+        ascii_string = chr(number_of_bytes_to_pad)
+        padding_str = number_of_bytes_to_pad * ascii_string
+        padded_plain_text = plain_text + padding_str
+        return padded_plain_text
+
+    @staticmethod
+    def __unpad(plain_text):
+        last_character = plain_text[len(plain_text) - 1:]
+        return plain_text[:-ord(last_character)]
+
+if __name__ == "__main__":
+    secret_key = get_random_bytes(8)
+    print(secret_key)
+    
+    print("hel")
+    enc = AESCipher(secret_key)
+    encrypted_text = enc.encrypt('avaneesh')
+    print(encrypted_text)
+    print(enc.decrypt(encrypted_text))
